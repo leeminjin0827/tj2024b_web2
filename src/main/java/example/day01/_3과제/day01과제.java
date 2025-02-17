6b3af810adac003e4330dcfa824e2e243a9a1bf0
leeminjin0827 <lmj98202@gmail.com>
2025-02-17 15:40:22 +0900

    day66 mapping

---
+++ src/main/java/example/day01/_3과제/day01과제.java
@@ -0,0 +1,61 @@
+package example.day01._3과제;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/day01")
+public class day01과제 {
+    // 글쓰기
+    @PostMapping("/bpost")
+    public boolean bPost(){
+        System.out.println("day01 bpost!!");
+        return true;
+    } // f end
+
+    // 전체 글 조회
+    @GetMapping("/bget1")
+    public List<Map<String, String>> bGet1(){
+        System.out.println("day01 bget1!!");
+        List<Map<String , String > > list = new ArrayList<>();
+        Map<String , String > map1 = new HashMap<>();
+        map1.put("bno" , "1" );
+        map1.put("btitle" , "테스트1" );
+        list.add(map1);
+        Map<String , String > map2 = new HashMap<>();
+        map2.put("bno" , "2" );
+        map2.put("btitle" , "테스트2" );
+        list.add(map2);
+        return list;
+    } // f end
+
+    // 개별 글 조회
+    @GetMapping("/bget2")
+    public Map<String,String> bGet2(){
+        System.out.println("day01 bget2!!");
+        Map<String ,String> map = new HashMap<>();
+        map.put("bno" , "1");
+        map.put("btitle" , "테스트" );
+        return map;
+    } // f end
+
+    // 개별 글 수정
+    @PutMapping("/bput")
+    public boolean bPut(){
+        System.out.println("day01 bput!!");
+        return true;
+    } // f end
+
+    // 개별 글 삭제
+     @DeleteMapping("/bdelete")
+    public int bDelete(){
+        System.out.println("day01 bdelete!!");
+        return 1;
+    } // f end
+
+
+} // c end
