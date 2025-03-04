0452fb83df038f4d7443d1753bfc7826d7757c7e
leeminjin0827 <lmj98202@gmail.com>
2025-03-04 15:27:50 +0900

    react 리액트 구조 / 컴포넌트

---
+++ src/main/reactapp/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+// react18
+// (!) 기본코드 주석처리
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// (*) index.html에 root DOM 가져오기.
+const root = createRoot( document.querySelector('#root') )
+
+// day01 : '컴포넌트 만들기' 렌더링
+  // import 호출할컴포넌트명 from `경로/파일명(확장자)';
+import Component1 from './example/1_동작구조/Component1.jsx';
+// root.render( <Component1 /> )
+
+// day01 : 여러 컴포넌트 만들기
+import Component2 from './example/1_동작구조/Component2.jsx'
+root.render( <Component2 /> )
